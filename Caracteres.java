9d72bbb01fe7eb5204f3591f54ebbe9ba1cb97a8
marianaVsa <20233tn234@utez.edu.mx>
2025-05-08 08:43:15 -0600

    Imprimir el char letra

---
+++ Caracteres.java
@@ -0,0 +1,10 @@
+public class Caracteres {
+    public static void main(String[] args){
+        char letra = 'a';
+        char simbolo = '$';
+        char numeroChar = '5';
+
+        System.out.println("letra =" + letra);
+      
+    }
+}
